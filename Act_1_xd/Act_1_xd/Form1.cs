1b5b34d76a4e16efda46591ceedc828a33d90b53
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-22 19:51:08 -0600

    Add files via upload
    Actividad realizada en clase, sobre la ilusión 3D

---
+++ Act_1_xd/Act_1_xd/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act_1_xd
+{
+    public partial class Form1 : Form
+    {
+        Canvas canvas;
+
+        Bitmap bmp;
+        Graphics g;
+
+        static float delta;
+        static DateTime lastTime, currentTime;
+        static int framesRendered = 0;
+        public static int fps;
+
+        Space f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            canvas = new Canvas(bmp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f = new Space(3000, 150, 250);
+            lastTime = DateTime.Now;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            framesRendered++;
+            currentTime = DateTime.Now;
+            if ((currentTime - lastTime).TotalSeconds >= 1)
+            {
+                // one second has elapsed
+                fps = framesRendered;
+                framesRendered = 0;
+                delta = (float)(currentTime - lastTime).TotalMilliseconds / 1000000;
+                lastTime = currentTime;
+            }
+            f.Render(canvas, delta);
+            label1.Text = fps.ToString();
+            pictureBox1.Refresh();
+        }
+    }
+}
---
+++ Act_1_xd/Act_1_xd/Space.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act_1_xd
+{
+    public class Space
+    {
+        private float spread, speed;
+        Random rand;
+        float[] x, y, z;
+
+        public Space(int stars, float spread, float speed)
+        {
+            this.speed = speed;
+            this.spread = spread;
+
+            x= new float[stars];
+            y= new float[stars];
+            z= new float[stars];
+            
+            rand= new Random();
+
+            for(int i=0; i<stars; i++) 
+            {
+                Init(i);
+            }
+        }
+        public void Init(int index)
+        {
+            x[index] = 2 * ((float)rand.NextDouble() - 0.5f) * spread;
+            y[index] = 2 * ((float)rand.NextDouble() - 0.5f) * spread;
+            z[index] = 2 * ((float)rand.NextDouble() + 0.00001f) * spread;
+
+        }
+
+        public void Render(Canvas target, float delta)
+        {
+            target.Clear();
+
+            float hWidht = target.Width / 2f;
+            float hHeight = target.Height / 2f;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                z[i] -= delta * speed;
+                if (z[i] <= 0)
+                    Init(i);
+
+                int xPos = (int)(x[i]/z[i] * hWidht + hWidht);
+                int yPos = (int)(y[i]/z[i] * hHeight + hHeight);
+
+                if (xPos < 0 || xPos >= target.Width || yPos < 0 || yPos >= target.Height)
+                {
+                    Init(i);
+                }
+                else
+                    target.PutPixel(xPos, yPos, Color.DarkSeaGreen);
+            }
+        }
+    }
+}
