bfde18b37e32a48dcc8b37823676e19f9f8b8ca8
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-22 19:48:28 -0600

    Add files via upload
    Actividad de la rotación 2D

---
+++ Act_2/Act_2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Act_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 17F);
+            this.button1.Location = new System.Drawing.Point(229, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rotar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(229, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 250);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 17F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(541, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grados";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Impact", 17F);
+            this.textBox1.Location = new System.Drawing.Point(364, 369);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 35);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Impact", 17F);
+            this.button2.Location = new System.Drawing.Point(229, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Posición";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Act_2/Act_2/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act_2
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap bmp;
+        static Graphics g;
+
+        Point a, b, c, d; // Cuadrado 1
+        Point r, t, f, h; // Cuadrado 2
+        Point u, i, o, p; // Cuadrado 3
+        Point x, y, z, w; // Rotación
+
+        double angle;
+        Point pivot;
+        bool sqr1Selected = true;
+        bool sqr2Selected = true;
+        bool sqr3Selected = true;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sqr1Selected)
+            {
+                sqr1Selected = false;
+                sqr2Selected = true;
+                sqr3Selected = false;
+            }
+            else if (sqr2Selected)
+            {
+                sqr1Selected = false;
+                sqr2Selected = false;
+                sqr3Selected = true;
+            }
+            else if (sqr3Selected)
+            {
+                sqr1Selected = true;
+                sqr2Selected = false;
+                sqr3Selected = false;
+            }
+            g.Clear(Color.Black);
+            Renderplane();
+            if (sqr1Selected)
+            {
+                RenderSqr(a, b, c, d);
+            }
+            else if (sqr2Selected)
+            {
+                RenderSqr2(r, t, f, h);
+            }
+            else if (sqr3Selected)
+            {
+                RenderSqr3(u, i, o, p);
+            }
+            /*sqr1Selected = !sqr1Selected;
+            g.Clear(Color.Black);
+            Renderplane();
+            if (sqr1Selected)
+            {
+                RenderSqr(a, b, c, d);
+            }
+            else
+            {
+                RenderSqr2(r, t, f, h);
+            }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text = textBox1.Text;
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+            g.Clear(Color.Black);
+            Renderplane();
+            if (sqr1Selected)
+            {
+                Rota1();
+                RenderSqr(x, y, z, w);
+            }
+            else if (sqr2Selected)
+            {
+                Rota2();
+                RenderSqr2(x, y, z, w);
+            }
+            else if (sqr3Selected)
+            {
+                Rota3();
+                RenderSqr3(x, y, z, w);
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            pivot = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            Renderplane();
+
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            a = new Point(centerX, centerY);
+            b = new Point(centerX + 50, centerY);
+            c = new Point(centerX + 50, centerY - 50);
+            d = new Point(centerX, centerY - 50);
+
+            RenderSqr(a, b, c, d);
+
+            r = new Point(centerX - 25, centerY - 25);
+            t = new Point(centerX + 25, centerY - 25);
+            f = new Point(centerX + 25, centerY + 25);
+            h = new Point(centerX - 25, centerY + 25);
+
+            RenderSqr2(r, t, f, h);
+
+            u = new Point(centerX + 5, centerY - 35);
+            i = new Point(centerX + 55, centerY - 35);
+            o = new Point(centerX + 55, centerY + 15);
+            p = new Point(centerX + 5, centerY + 15);
+
+            RenderSqr3(u, i, o, p);
+
+        }
+
+        private void Renderplane()
+        {
+            g.DrawLine(Pens.Yellow, new Point(pictureBox1.Width / 2, 0), new Point(pictureBox1.Width / 2, pictureBox1.Height));
+            g.DrawLine(Pens.Yellow, new Point(0, pictureBox1.Height / 2), new Point(pictureBox1.Width, pictureBox1.Height / 2));
+        }
+        private void RenderSqr(Point a, Point b, Point c, Point d)
+        {
+            g.DrawLine(Pens.Red, a, b);
+            g.DrawLine(Pens.Red, b, c);
+            g.DrawLine(Pens.Red, c, d);
+            g.DrawLine(Pens.Red, d, a);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void RenderSqr2(Point r, Point t, Point f, Point h)
+        {
+            g.DrawLine(Pens.Red, r, t);
+            g.DrawLine(Pens.Red, t, f);
+            g.DrawLine(Pens.Red, f, h);
+            g.DrawLine(Pens.Red, h, r);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void RenderSqr3(Point u, Point i, Point o, Point p)
+        {
+            g.DrawLine(Pens.Red, u, i);
+            g.DrawLine(Pens.Red, i, o);
+            g.DrawLine(Pens.Red, o, p);
+            g.DrawLine(Pens.Red, p, u);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void Rota1()
+        {
+            x.X = (int)(((a.X - pivot.X) * Math.Cos(angle)) - ((a.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            x.Y = (int)(((a.X - pivot.X) * Math.Sin(angle)) + ((a.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            y.X = (int)(((b.X - pivot.X) * Math.Cos(angle)) - ((b.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            y.Y = (int)(((b.X - pivot.X) * Math.Sin(angle)) + ((b.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            z.X = (int)(((c.X - pivot.X) * Math.Cos(angle)) - ((c.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            z.Y = (int)(((c.X - pivot.X) * Math.Sin(angle)) + ((c.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            w.X = (int)(((d.X - pivot.X) * Math.Cos(angle)) - ((d.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            w.Y = (int)(((d.X - pivot.X) * Math.Sin(angle)) + ((d.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+        }
+        private void Rota2()
+        {
+            x.X = (int)(((r.X - pivot.X) * Math.Cos(angle)) - ((r.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            x.Y = (int)(((r.X - pivot.X) * Math.Sin(angle)) + ((r.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            y.X = (int)(((t.X - pivot.X) * Math.Cos(angle)) - ((t.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            y.Y = (int)(((t.X - pivot.X) * Math.Sin(angle)) + ((t.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            z.X = (int)(((f.X - pivot.X) * Math.Cos(angle)) - ((f.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            z.Y = (int)(((f.X - pivot.X) * Math.Sin(angle)) + ((f.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            w.X = (int)(((h.X - pivot.X) * Math.Cos(angle)) - ((h.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            w.Y = (int)(((h.X - pivot.X) * Math.Sin(angle)) + ((h.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+        }
+        private void Rota3()
+        {
+            x.X = (int)(((u.X - pivot.X) * Math.Cos(angle)) - ((u.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            x.Y = (int)(((u.X - pivot.X) * Math.Sin(angle)) + ((u.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            y.X = (int)(((i.X - pivot.X) * Math.Cos(angle)) - ((i.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            y.Y = (int)(((i.X - pivot.X) * Math.Sin(angle)) + ((i.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            z.X = (int)(((o.X - pivot.X) * Math.Cos(angle)) - ((o.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            z.Y = (int)(((o.X - pivot.X) * Math.Sin(angle)) + ((o.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            w.X = (int)(((p.X - pivot.X) * Math.Cos(angle)) - ((p.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            w.Y = (int)(((p.X - pivot.X) * Math.Sin(angle)) + ((p.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+        }
+    }
+}
